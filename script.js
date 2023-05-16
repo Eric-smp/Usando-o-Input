f363719df0489d32413d4495bea34e50c7777e28
Eric Sampaio <eric.sampaio@devbot.com.br>
2023-05-16 14:47:56 -0300

    Atualização no layout e js

--- script.js
+++ script.js
@@ -1,56 +1,79 @@
-/* 
-
-function total(){
+/* function total(){
   const peraValor = document.getElementById('pera').value
   const bananaValor = document.getElementById('banana').value
-  
-  const frutas = [{fruta:'pera', valor:2.50}, {fruta:'banana', valor:3.50 }]
-  
-  const selecPera = frutas.filter(function(item)  {
-    return item.valor === 2.50 
-    const resutPera = (item.valor === 2.50) ** peraValor
-    console.log(resutPera)
-  })
-  console.log(selecPera)
-  const resultadoPera = document.getElementById('resultado').innerHTML= selecPera * peraValor
- 
-  
-  
-  
-  
-} */
-
-
-
-function total(){
-  const peraValor = document.getElementById('pera').value
-  const bananaValor = document.getElementById('banana').value
-  
-  
-  const frutas = [{fruta:'pera', valor:2.50}, {fruta:'banana', valor:3.50}]
-  console.log(frutas)
-
-
 
   
-  const filterFruits = frutas.filter((item)=>{
-    if(item.fruta === 'pera'){
-      return item
-      
-    }else if(item.fruta === 'banana'){
-      return item
-      
-    }
-  })
+  
   const resultPera=filterFruits[0].valor * parseFloat(peraValor)
   const resultBanana=filterFruits[1].valor * parseFloat(bananaValor)
   document.getElementById('resumo').innerHTML=resultPera
   document.getElementById('resultado').innerHTML=resultBanana
 
   document.getElementById('resultadoTotal').innerHTML=(resultBanana) + (resultPera)
-  
-  
-
-  
-
 }
+
+
+
+
+
+function alimentos(item){
+  const frutas = [{fruta:'pera', id:3}, {fruta:'banana', id:2.50}, {fruta:'manga', id:4.50}, {fruta:'melancia', id:7}, {fruta:'abacate', id:2}, {fruta:'laranja', id:3.50} ]
+
+  const filterFrutas = frutas.filter((item)=>{
+    if(item.fruta === 'pera'){
+      return 
+    }
+
+  })
+  
+  
+
+
+
+} */
+
+
+/* 
+
+
+
+const numeros = [2,4,6,8,10];
+const promises = [];
+numeros.forEach(entry => promises.push(dividePelaMetade(entry)));
+Promise.all(promises)
+    .then(results => results.forEach(entry => console.log(entry)))
+    .catch(error => console.log(error));
+ 
+console.log("teste");
+
+
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
